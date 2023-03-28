672b59d6cf379364d0af4774b9f5097875b7f531
Rachel Lea Kohen <73036510+rkohen770@users.noreply.github.com>
2023-03-28 20:15:24 +0000

    hello world!

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
+
+//figure out why this doesnt work:
+const element = <h1>this will be the home page</h1>;
+ReactDOM.render(element, document.getElementById("root"));
+
+
+//this will be the home page, and from here you can go to the text editor or get to 100 game
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
