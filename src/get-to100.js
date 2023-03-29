e4e58d89e267313f97ea8e4a3e2ac6f41a3bee4e
shiffy01 <sscheine@g.jct.ac.il>
2023-03-29 17:47:10 +0300

    changes

--- src/get-to100.js
+++ src/get-to100.js
@@ -1,1 +1,9 @@
 //get to 100 game
+import React from "react";
+import ReactDOM from "react-dom";
+import signIn from "./signinform";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<signIn />);
+  
+//hi
---
+++ src/signInForm.jsx
@@ -0,0 +1,16 @@
+//import { Component } from "react"
+import React, { Component } from 'react';
+class signIn extends Component {
+
+    render() { 
+        return (
+        <form>
+            <label>Enter your name:
+            <input type="text" />
+            </label>
+      </form>
+        );
+    }
+}
+ 
+export default signIn;
