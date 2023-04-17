ad20ed2b966e21984025ade2cebbbf9dc767760c
rachel lea kohen <rkohen@g.jct.ac.il>
2023-04-17 20:08:58 +0300

    folders

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
-import TextEditor from "./TextEditor";
-import { Component } from "react";
-=======
 import App from "./App";
 
->>>>>>> c4611013bc13bf942dcd11bfc0852e253ff55468
 // import "./index.css";
 
 ReactDOM.render(<App/>, document.getElementById("root"));
