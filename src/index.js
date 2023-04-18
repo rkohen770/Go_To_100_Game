ad05548f2d1be7172e9dbf3e4f4d15f8ee04abc4
rachel lea kohen <rkohen@g.jct.ac.il>
2023-04-19 00:42:36 +0300

    hebrew keyboard

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import TextEditor from "./js/TextEditor";
 //import game from "./100game";
 import Tryout from "./Tryout";
 // import "./index.css";
-const elm= <h1>stay happy</h1>
-ReactDOM.render(<Tryout/>, document.getElementById("root"));
+const elm = <h1>stay happy</h1>;
+ReactDOM.render(<Tryout />, document.getElementById("root"));
 
 //text editor component
-//ReactDOM.render(<TextEditor />, document.getElementById("root"));
-
-
-
-
+ReactDOM.render(<TextEditor />, document.getElementById("root"));
